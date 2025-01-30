ec77a9ba666fc6f5e47272035974bf3c87f59ce4
Elisha-bit <elisha.tetteh@dtiafrica.edu.gh>
2025-01-30 20:22:24 +0000

    Add files via upload

--- java.js
+++ java.js
@@ -1,85 +1,48 @@
-let boxes = document.querySelectorAll(".box")
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
 
-let turn = "X";
-let isGameOver = false;
+let player;
+let computer;
+let result;
 
-boxes.forEach(e=>{
-    e.innerHTML = ""
-    e.addEventListener("click", ()=>{
-        if(!isGameOver && e.innerHTML ===""){
-            e.innerHTML = turn;
-            cheakWin();
-            cheakDraw();
-            changeTurn();
-        }
-    })
-})
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+    player =button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+}));
 
-function changeTurn(){
-    if(turn === "X"){
-        turn = "O";
-        document.querySelector(".bg").computedStyleMap.left ="85px"
-    }
-    else{
-          turn = "X";
-        document.querySelector(".bg").computedStyleMap.left ="0";    
-    }
-}
+function computerTurn(){
+    const randNum =Math.floor(Math.random() * 3) + 1;
 
-function cheakDraw(){
-    if(!isGameOver){
-        let isDraw = true;
-        boxes.forEach(e =>{
-            if(e.innerHTML ==="") isDraw = false;
-        })
-        if(isDraw) {
-            isGameOver = true;
-            document.querySelector("#results").innerHTML = " Draw";
-            document.querySelector("#play-again").style.display = "inline";   
-        }
+
+    switch(randNum){
+        case 1:
+            computer = "ROCK";
+            break;
+        case 2:
+             computer = "PAPER";
+              break; 
+        case 3:
+             computer = "SCISSORS";
+             break;   
     }
 }
 
-document.querySelector("#play-again").addEventListener("click",()=>{
-    isGameOver = false;
-    turn = "X";
-    document.querySelector(".bg").style.left = "0";
-    document.querySelector("#results").innerHTML = "";
-    document.querySelector("#play-again").style.display = "none";
-
-
-    boxes.forEach(e =>{
-        e.innerHTML = "";
-        e.style.removeproperty("background-color");
-        e.style.color = "#fff";
-    })
-})
-
- function cheakWin(){
-    let winConditions =[
-        [0,1,2], [3,4,5], [6,7,8],
-        [0,3,6], [1,4,7], [2,5,8],
-        [0,4,8], [2,4,6]
-    ]
-    for(let i = 0; i<winConditions.length; i++){
-        let v0 = boxes[winConditions[i][0]].
-        innerHTML;
-        let v1 = boxes[winConditions[i][1]].
-        innerHTML;
-        let v2 = boxes[winConditions[i][2]].
-        innerHTML;
-
-        if(v0 !="" && v0 === v1 && v0 === v2){
-            isGameOver = true;
-            document.querySelector("#results").innerHTML = turn + " win";
-            document.querySelector("#play-again").style.display = "inline";
-
-            for(j = 0; j<3; j++){
-                boxes[winConditions[i][j]].style.
-                backgroundColor = "#08D9D6"
-                boxes[winConditions[i][j]].style.
-                backgroundColor = "#000"
-            }
-        }
+function checkWinner(){
+    if(player == computer){
+        return "Draw!"
     }
- }
+    else if(computer == "ROCK"){
+        return(player == "PAPER") ? "You Win!" : "You Lose!"
+    }
+    else if(computer == "PAPER"){
+        return(player == "SCISSORS") ? "You Win!" : "You Lose!"
+    }
+    else if(computer == "SCISSORS"){
+        return(player == "ROCKS") ? "You Win!" : "You Lose!"
+    }
+}
